32bec01c8143e15918d932e7f1f2e86706e509f1
Grant Fritchey <grant@scarydba.com>
2022-12-06 14:30:20 +0000

    Updates for new article
    Renamed a file because I want the CreateDatabase file to be first

--- IndexTypes.sql
+++ IndexTypes.sql
@@ -1,23 +1,36 @@
+--defaults
 CREATE INDEX radios_radioname ON radios(radioname);
 
+--defining index type
 CREATE INDEX radios_radioname ON radios USING BTREE(radioname);
 
 DROP INDEX if EXISTS radios_radioname;
 
+--compound keys
 CREATE INDEX radios_radioname_radioid ON radios USING BTREE(radioname, radioid);
 
 DROP INDEX IF EXISTS radios_radioname_radioid;
 
-CREATE INDEX radios_radioid_unique ON radios USING BTREE(radioid);
+--uniqe
+CREATE UNIQUE INDEX radios_radioid_unique ON radios USING BTREE(radioid);
 
 DROP INDEX IF EXISTS radios_radioid_unique;
 
+--defining order
 CREATE INDEX radios_radioname ON radios USING BTREE(radioname DESC NULLS FIRST);
 
 DROP INDEX IF EXISTS radios_radioname;
 
+--include
 CREATE INDEX radios_radioid ON radios USING BTREE(radioid) INCLUDE(radioname);
 
 DROP INDEX IF EXISTS radios_radioid;
+
+--partial/filtered
+CREATE INDEX radios_radioname ON radios USING BTREE(radioname)
+WHERE radioname IS NOT NULL;
+
+
+
 
 create index on radios using btree(radioid)
