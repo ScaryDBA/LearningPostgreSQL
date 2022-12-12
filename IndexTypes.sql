7ed4ea230b8971877a59a4d84774e96ce5488abe
Grant Fritchey <grant@scarydba.com>
2022-12-12 13:10:02 -0600

    Necessary changes

--- IndexTypes.sql
+++ IndexTypes.sql
@@ -1,36 +1,39 @@
 --defaults
-CREATE INDEX radios_radioname ON radios(radioname);
+CREATE INDEX radios_radioname ON radio.radios(radio_name);
 
 --defining index type
-CREATE INDEX radios_radioname ON radios USING BTREE(radioname);
+CREATE INDEX radios_radioname ON radio.radios USING BTREE(radio_name);
 
-DROP INDEX if EXISTS radios_radioname;
+DROP INDEX if EXISTS radio.radios_radioname;
 
 --compound keys
-CREATE INDEX radios_radioname_radioid ON radios USING BTREE(radioname, radioid);
+CREATE INDEX radios_radioname_radioid ON radio.radios USING BTREE(radio_name, radio_id);
 
-DROP INDEX IF EXISTS radios_radioname_radioid;
+DROP INDEX IF EXISTS radio.radios_radioname_radioid;
 
 --uniqe
-CREATE UNIQUE INDEX radios_radioid_unique ON radios USING BTREE(radioid);
+CREATE UNIQUE INDEX radios_radioid_unique ON radio.radios USING BTREE(radio_id);
 
-DROP INDEX IF EXISTS radios_radioid_unique;
+DROP INDEX IF EXISTS radio.radios_radioid_unique;
 
 --defining order
-CREATE INDEX radios_radioname ON radios USING BTREE(radioname DESC NULLS FIRST);
+CREATE INDEX radios_radioname ON radio.radios USING BTREE(radio_name DESC NULLS FIRST);
 
-DROP INDEX IF EXISTS radios_radioname;
+DROP INDEX IF EXISTS radio.radios_radioname;
 
 --include
-CREATE INDEX radios_radioid ON radios USING BTREE(radioid) INCLUDE(radioname);
+CREATE INDEX radios_radioid ON radio.radios USING BTREE(radio_id) INCLUDE(radio_name);
 
-DROP INDEX IF EXISTS radios_radioid;
+DROP INDEX IF EXISTS radio.radios_radioid;
 
 --partial/filtered
-CREATE INDEX radios_radioname ON radios USING BTREE(radioname)
-WHERE radioname IS NOT NULL;
+CREATE INDEX radios_radioname ON radio.radios USING BTREE(radio_name)
+WHERE radio_name IS NOT NULL;
 
 
+drop index if exists radio.radios_radioname;
 
 
-create index on radios using btree(radioid)
+--Hash Indexes
+
+CREATE INDEX bands_band_name ON radio.bands USING HASH(band_name);
