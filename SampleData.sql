458cc7767ca27b6ac22262f7438a3d9217fbf202
Grant Fritchey <grant@scarydba.com>
2022-12-12 12:20:39 -0600

    DB & Sample Complete

--- SampleData.sql
+++ SampleData.sql
@@ -28,5 +28,43 @@ VALUES
 INSERT INTO radio.antenna
 (antenna_name,manufacturer_id,connectortype_id)
 VALUES
-('Rubber duck')
+('Rubber duck',3,2),
+('Rubber duck',2,3),
+('Multi-band',1,4);
 
+INSERT INTO radio.antennabands
+(band_id,antenna_id)
+VALUES
+(1,1),(2,1),(4,1),(5,1),
+(1,2),(2,2),(4,2),(5,2),
+(3,3),(5,3);
+
+INSERT INTO radio.digitalmodes
+(digitalmode_name)
+VALUES
+('FusionX'),('DMR');
+
+INSERT INTO radio.radios
+(radio_name,manufacturer_id,connectortype_id,digitalmde_id)
+VALUES
+('FT3D'),2,2,1),('UV5R',3,3,NULL),('G90',6,4,NULL);
+
+INSERT INTO radio.radioantenna
+(radio_id,antenna_id)
+VALUES
+(1,2),(2,1),(3,3);
+
+
+INSERT INTO logging.logs
+(log_date,log_callsign,log_location)
+VALUES
+(CURRENT_TIMESTAMP,'KC1KCE','35.947, -96.149'),
+(CURRENT_TIMESTAMP,'KC1KCE','35.963, -96.114'),
+(CURRENT_TIMESTAMP,'KC1KCE','35.985, -96.106');
+
+INSERT INTO logging.parksontheair
+(contacts)
+VALUES
+(ARRAY['KC1KCE', 'KI5EGH', 'KJ7VSF','N4DAD','KD7IFN']),
+(ARRAY['KN4XP','KA0GAV','NM8GS','W3DET']),
+(ARRAY['W3DET','N4AUG','KC3WRX','WA9EBX','W1OW','K4RFH','W9DC','WJ8L','N4HES'])
